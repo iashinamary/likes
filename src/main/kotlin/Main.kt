a6aee452d9de0db2767e7302be536b14ac8a1ae9
Mariia Iashina <iashinamary@gmail.com>
2023-11-05 23:01:15 +0300

    Исправлены ошибки

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 fun main() {
-    val likes = 61
+    val likes = 311
     val lastDigit = likes % 10
 
-    if (likes.equals(11)){
+    if (likes % 100 == 11){
         println("Нравится " + likes + " людям")
     } else if (lastDigit.equals(1)){
         println("Нравится " + likes + " человеку")
